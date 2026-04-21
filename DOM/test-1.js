ac6d4f6df6ce3eaf0dc6389e12172c6dab8e67bd
SamiumBashir <samiumbashirbosunia@gmail.com>
2026-04-21 18:02:50 +0600

    added style using DOM

--- DOM/test-1.js
+++ DOM/test-1.js
@@ -3,10 +3,18 @@ firstOne.innerText = 'find 10'
 console.log(firstOne);
 
 const secondOne = document.getElementById('second-heading');
-const boxi = document.getElementById ('box')
 secondOne.textContent = 'amra sobai raja amader ai rajar rajotte'
-const newPara = document.createElement('p')
+
+secondOne.style.backgroundColor = 'blue'
+secondOne.style.color = 'white'
+secondOne.style.textAlign = 'center'
+secondOne.style.padding = '10px 5px'
+secondOne.style.borderRadius = '10px'
+
+const boxi = document.getElementById ('box')
 newPara.innerText = 'happy happy method'
+
+const newPara = document.createElement('p')
 boxi.appendChild(newPara);
 console.log(newPara);
 console.log(secondOne);
