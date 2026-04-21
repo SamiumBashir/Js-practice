ce77fde78c27cbbc7f09ec91b9840bfd59482ac6
SamiumBashir <samiumbashirbosunia@gmail.com>
2026-04-21 17:27:53 +0600

    modified the h3

--- DOM/test-1.js
+++ DOM/test-1.js
@@ -1,3 +1,7 @@
 const firstOne = document.getElementById('btnOne');
 firstOne.innerText = 'find 10'
 console.log(firstOne);
+
+const secondOne = document.getElementById('second-heading');
+secondOne.textContent = 'amra sobai raja amader ai rajar rajotte'
+console.log(secondOne);
