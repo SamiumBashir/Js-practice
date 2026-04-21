94c9ee4a51317759d8ea2ac4ce0f593822d5a81b
SamiumBashir <samiumbashirbosunia@gmail.com>
2026-04-21 17:48:02 +0600

    more practice on js

--- DOM/test-1.js
+++ DOM/test-1.js
@@ -3,5 +3,10 @@ firstOne.innerText = 'find 10'
 console.log(firstOne);
 
 const secondOne = document.getElementById('second-heading');
+const boxi = document.getElementById ('box')
 secondOne.textContent = 'amra sobai raja amader ai rajar rajotte'
+const newPara = document.createElement('p')
+newPara.innerText = 'happy happy method'
+boxi.appendChild(newPara);
+console.log(newPara);
 console.log(secondOne);
