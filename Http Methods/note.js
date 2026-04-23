0c960993c5cf9ffa56b632d6e0cfb67f90543165
SamiumBashir <samiumbashirbosunia@gmail.com>
2026-04-23 19:53:30 +0600

    notes om https methods

---
+++ Http Methods/note.js
@@ -0,0 +1,22 @@
+
+
+// GET - সার্ভার থেকে ডাটা Retrieve করার জন্য ব্যবহার করা হয়।
+// POST - কোনো Data কে সার্ভারে পাঠানোর জন্য ব্যবহার করা হয়।
+// PUT - কোনো একটা existing resource -এ নতুন resourse দ্বারা replace করার জন্য ব্যবহার করা হয়। 
+// PATCH - কোনো একটা resource এর আংশিক modification এর জন্য ব্যবহার করা হয়। 
+// DELETE - নিদির্ষ্ট কিছুকে remove করার জন্য DELETE ব্যবহার করা হয়।
+
+
+//Structure of an HTTP Request
+// 1. Request line
+//     i. The Methods
+//     ii. The Path 
+//     iii. The HTTP Version 
+
+// 2. Headers
+//     i.Key-Value pairs , contain metadata
+
+// 3. Empty Line
+//      * A mandatory blank line separating headers from body
+
+// 4. Message Body 
